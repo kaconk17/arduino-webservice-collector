0c0317aae9934b02d020934de2d88e667c6535d7
kaconk <harutonouciha@gmail.com>
2022-12-30 21:40:25 +0700

    client only

--- index.js
+++ index.js
@@ -1,78 +1,43 @@
-const aedes = require('aedes')()
-const server = require('net').createServer(aedes.handle)
+const mqtt = require('mqtt'); 
 require('dotenv').config();
 
 const port = process.env.MQTT_PORT;
 const broker_host = process.env.MQTT_SERVER;
 const user_name = process.env.MQTT_USER;
 const passwd = process.env.MQTT_PASSWORD;
+const idClient = process.env.MQTT_CLIENTID;
+
+const options={
+  clientId:idClient,
+  username: user_name,
+  password:passwd,
+  clean:true
+};
+
+const client = mqtt.connect(broker_host, options);
+
+client.on("connect",function(){
+  console.log("Connected to broker");
+});
 
 const {saveTemp, devUpd, checkDev, savePower} = require('./app/controllers/dataController');
 
-server.listen(port, function () {
-    console.log('server started and listening on port ', port)
-  })
-
-// authenticate the connecting client
-aedes.authenticate = (client, username, password, callback) => {
-  password = Buffer.from(password, 'base64').toString();
-  if (username === user_name && password === passwd) {
-      return callback(null, true);
+client.on("message",function(topic,message,packet){
+  var strtopic = topic.split("/");
+  var clientID = strtopic[1];
+  
+  if (strtopic[0] == 'temp') {
+    var tempVal = Buffer.from(message,'base64').toString();
+    saveTemp(clientID,tempVal);
   }
-  const error = new Error('Authentication Failed!! Invalid user credentials.');
-  console.log('Error ! Authentication failed.')
-  return callback(error, false)
-}
-
-// authorizing client to publish on a message topic
-aedes.authorizePublish = (client, packet, callback) => {
-  var topics =[
-    "temp",
-    "power"
-  ];
-  var intopic = packet.topic.split("/");
-  if(topics.includes(intopic[0])){
-    
-    return callback(null);
+  if (strtopic[0] == 'power') {
+    var jsonpayload = JSON.parse(message);
+    //console.log(jsonpayload);
+    savePower(clientID,jsonpayload);
   }
-  console.log('Error ! Unauthorized publish to a topic.')
-  return callback(new Error('You are not authorized to publish on this message topic.'));
-}
+  
 
-// emitted when a client connects to the broker
-aedes.on('client', function (client) {
-  devUpd(client.id,'ON');
-  console.log(`[CLIENT_CONNECTED] Client ${(client ? client.id : client)} connected to broker ${aedes.id}`)
-})
+});
+client.subscribe("temp/#",{qos:1});
+client.subscribe("power/#",{qos:1});
 
-// emitted when a client disconnects from the broker
-aedes.on('clientDisconnect', function (client) {
-  devUpd(client.id,'OFF');
-  console.log(`[CLIENT_DISCONNECTED] Client ${(client ? client.id : client)} disconnected from the broker ${aedes.id}`)
-})
-
-// emitted when a client subscribes to a message topic
-aedes.on('subscribe', function (subscriptions, client) {
-  console.log(`[TOPIC_SUBSCRIBED] Client ${(client ? client.id : client)} subscribed to topics: ${subscriptions.map(s => s.topic).join(',')} on broker ${aedes.id}`)
-})
-
-// emitted when a client unsubscribes from a message topic
-aedes.on('unsubscribe', function (subscriptions, client) {
-  console.log(`[TOPIC_UNSUBSCRIBED] Client ${(client ? client.id : client)} unsubscribed to topics: ${subscriptions.join(',')} from broker ${aedes.id}`)
-})
-
-// emitted when a client publishes a message packet on the topic
-aedes.on('publish', async function (packet, client) {
-  if (client) {
-    if (packet.topic.includes('temp')) {
-      var tempVal = Buffer.from(packet.payload,'base64').toString();
-      saveTemp(client.id,tempVal);
-    }
-    if (packet.topic.includes('power')) {
-      var jsonpayload = JSON.parse(Buffer.from(packet.payload,'base64'));
-      savePower(client.id,jsonpayload);
-    }
-      console.log(`[MESSAGE_PUBLISHED] Client ${(client ? client.id : 'BROKER_' + aedes.id)} has published message on ${packet.topic} to broker ${aedes.id}`)
-      
-  }
-})
